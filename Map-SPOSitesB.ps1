706cb42b7f69e19527bf3ca7b495dd2132353e80
grimothy <charlesjcoulter@gmail.com>
2024-06-04 14:27:27 -0400

    re-worked script

--- Map-SPOSitesB.ps1
+++ Map-SPOSitesB.ps1
@@ -1,23 +1,35 @@
-$userEmail = $(whoami /upn | Out-String).Trim()
-$siteID = "{72c2c06b-d443-4da8-a5f6-f2e072dd83b8}"
+# User's Office 365 email address
+$userEmail = (whoami /upn | Out-String).Trim()
+
+# SharePoint Online site details
+$siteID = "72c2c06b-d443-4da8-a5f6-f2e072dd83b8"
 $tenantName = "First Bank of the Lake"
-$webID = "{c0ac8667-2520-4edb-a35a-baf3a43e0aa8}"
+$webID = "c0ac8667-2520-4edb-a35a-baf3a43e0aa8"
 $webTitle = "FileIndex"
 $webUrl = "https://firstbanklake365.sharepoint.com/sites/FileIndex"
 
+# Import the list of SharePoint Online lists from a CSV file
 $SPOLIST = Import-Csv .\SPOLIST.csv
-$SPOLIST| ForEach-Object 
-{
-    $listId ="{'$_.ID'}"
-    $listTitle = $_.Title
-    if (-not ((Test-Path -Path "HKCU:\SOFTWARE\Microsoft\OneDrive\Accounts\Business1\Tenants\$tenantName") -and (Get-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\OneDrive\Accounts\Business1\Tenants\$tenantName" -Name "$env:USERPROFILE\$tenantName\$webTitle - $listTitle"))) {
-        $URL = "odopen://sync/?userEmail=" + [uri]::EscapeDataString($userEmail) + "&siteId=" + [uri]::EscapeDataString($siteID) + "&webId=" + [uri]::EscapeDataString($webID) + "&webTitle="+ [uri]::EscapeDataString($webTitle) + "&webUrl="+ [uri]::EscapeDataString($webUrl) + "&listId="+ [uri]::EscapeDataString($listId) + "&listTitle="+ [uri]::EscapeDataString($listTitle)
-        Start-Process "$env:LOCALAPPDATA\Microsoft\OneDrive\Onedrive.exe" -ArgumentList "/url:$URL"
+
+# Iterate over each list and sync if not already done
+foreach ($list in $SPOLIST) {
+    $listId = $list.ID
+    $listTitle = $list.Title
+    $registryPath = "HKCU:\SOFTWARE\Microsoft\OneDrive\Accounts\Business1\Tenants\$tenantName"
+    $propertyPath = "$env:USERPROFILE\$tenantName\$webTitle - $listTitle"
+
+    # Check if the list is already synced
+    if (-not (Test-Path -Path $registryPath) -and (Get-ItemProperty -Path $registryPath -Name $propertyPath)) {
+        # Construct the OneDrive sync URL
+        $URL = "odopen://sync/?userEmail=" + [uri]::EscapeDataString($userEmail) +
+               "&siteId=" + [uri]::EscapeDataString($siteID) +
+               "&webId=" + [uri]::EscapeDataString($webID) +
+               "&webTitle=" + [uri]::EscapeDataString($webTitle) +
+               "&webUrl=" + [uri]::EscapeDataString($webUrl) +
+               "&listId=" + [uri]::EscapeDataString($listId) +
+               "&listTitle=" + [uri]::EscapeDataString($listTitle)
+
+        # Start the OneDrive sync process
+        Start-Process "$env:LOCALAPPDATA\Microsoft\OneDrive\OneDrive.exe" -ArgumentList "/url:$URL"
     }
-
-}
-
-
-$SPOLIST| ForEach-Object {
-    Write-Host "SP DOC LIB IS " $_.Title  " and ID is " $_.Id
 }
